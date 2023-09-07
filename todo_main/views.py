8bcd991b53711ad529d2a824bcb462e687aa94d5
Sruthi Ravuru <sruthiravuru@gmail.com>
2023-09-07 21:47:53 +0200

    first commit of ToDo App

---
+++ todo_main/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from todos.models import Task
+def home(request):
+    tasks = Task.objects.filter(is_completed=False)
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'home.html', context)
